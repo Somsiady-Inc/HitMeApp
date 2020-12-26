8a91e19872606bd90ec35146308620aed6005fef
Zuravvski <m.zurawski1994@gmail.com>
2020-12-26 10:41:11 +0100

    Fixed weird formatting

--- backend/HitMeApp/HitMeApp.Users/Models/User.cs
+++ backend/HitMeApp/HitMeApp.Users/Models/User.cs
@@ -3,9 +3,6 @@
     internal class User
     {
         public string Email { get; protected init; }
-        public string Name
-        {
-            get; protected init;
-        }
+        public string Name { get; protected init; }
     }
 }
