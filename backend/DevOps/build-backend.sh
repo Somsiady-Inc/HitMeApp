4c84487e9ba2d8632680714a270ea46cc8f7ca31
Zuravvski <zurekseventyseven@gmail.com>
2021-01-03 23:32:29 +0100

    Contextualized devops shell scripts

--- backend/DevOps/build-backend.sh
+++ backend/DevOps/build-backend.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 LC_ALL=C
 
-# This script should be invoked from DevOps folder context
+# Folder Context Check
+CURRENT_FOLDER=$(pwd | xargs basename)
+
+if [[ ! "$CURRENT_FOLDER" = "DevOps" ]]
+then
+    echo "This script is support to be invoked from the DevOps folder context. Current context: '${CURRENT_FOLDER}'."
+    exit 1;
+fi
+
+# Actual Script
 ORIGINAL_PATH=$(pwd)
 
 [ -z "$1" ] && TAG=latest || TAG="$1"
