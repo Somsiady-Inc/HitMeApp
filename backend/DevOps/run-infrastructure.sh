8ab04390d0186c87eb5443d38b632f4608c07ef1
Michał <zurekseventyseven@gmail.com>
2021-01-06 17:20:22 +0100

    Contextualized devops shell scripts (#27)

--- backend/DevOps/run-infrastructure.sh
+++ backend/DevOps/run-infrastructure.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 LC_ALL=C
 
-docker-compose -f DevOps/infrastructure.yml down
-docker-compose -f DevOps/infrastructure.yml up --build -d
+# Folder Context Check
+CURRENT_FOLDER=$(pwd | xargs basename)
+
+if [[ ! "$CURRENT_FOLDER" = "DevOps" ]]
+then
+    echo "This script is support to be invoked from the DevOps folder context. Current context: '${CURRENT_FOLDER}'."
+    exit 1;
+fi
+
+# Actual Script
+docker-compose -f infrastructure.yml down
+docker-compose -f infrastructure.yml up --build -d
