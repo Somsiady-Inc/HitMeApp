d95aab2269f16c948b3d3856caab5f23c9e39b54
jacobSyb <jakubsybidlo@gmail.com>
2021-01-10 23:14:47 +0100

    Command for user login - without jwt

---
+++ backend/Modules/Identity/HitMeApp.Indentity/Application/Handlers/Commands/LoginUserHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using HitMeApp.Indentity.Application.Exceptions;
+using HitMeApp.Indentity.Application.Security;
+using HitMeApp.Indentity.Contract.Commands;
+using HitMeApp.Indentity.Core;
+using HitMeApp.Indentity.Core.Policies;
+using HitMeApp.Shared.Infrastructure.Cqrs.Commands;
+using Microsoft.AspNetCore.Identity;
+
+namespace HitMeApp.Indentity.Application.Handlers.Commands
+{
+    internal sealed class LoginUserHandler : ICommandHandler<LoginUser, Guid>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+        public LoginUserHandler(IUserRepository userRepository, IPasswordHasher<User> passwordHasher)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+        }
+
+        public async Task<Guid> Handle(LoginUser command)
+        {
+            var user = await _userRepository.Get(command.Id)
+                ?? throw new UserDoesNotExistException(command.Id);
+
+            var userPasswordService = new PasswordHasherBasedUserPasswordService(_passwordHasher, new DefaultPasswordStrengthPolicy());
+            if (!userPasswordService.Verify(user.Password, command.Password))
+                throw new InvalidCredentialsException();
+
+            await _userRepository.Save(user);
+            return user.Id;
+        }
+    }
+}
---
+++ backend/Modules/Identity/HitMeApp.Indentity/Contract/Commands/LoginUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HitMeApp.Indentity.Contract.Commands
+{
+    class LoginUser : IIdentityCommand<Guid>
+    {
+        public Guid Id { get; init; }
+        public string Email { get; init; }
+        public string Password { get; init; }
+    }
+}
