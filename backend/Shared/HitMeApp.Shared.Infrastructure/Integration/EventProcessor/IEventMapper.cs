07d792c0f93fbf577827609ce0120c10f650a652
Zuravvski <zurekseventyseven@gmail.com>
2021-01-25 21:23:48 +0100

    Added event processor

---
+++ backend/Shared/HitMeApp.Shared.Infrastructure/Integration/EventProcessor/IEventMapper.cs
@@ -0,0 +1,9 @@
+﻿using HitMeApp.Shared.DDD;
+
+namespace HitMeApp.Shared.Infrastructure.Integration.EventProcessor
+{
+    public interface IEventMapper
+    {
+        public IntegrationEvent Map(DomainEvent @event);
+    }
+}
---
+++ backend/Shared/HitMeApp.Shared.Infrastructure/Integration/IDomainEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using HitMeApp.Shared.DDD;
+
+namespace HitMeApp.Shared.Infrastructure.Integration
+{
+    public interface IDomainEventHandler<in TDomainEvent> where TDomainEvent : DomainEvent
+    {
+        public Task Handle(TDomainEvent @event);
+    }
+}
